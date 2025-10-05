92508186cecd40626fa047251362d29c80d3d34e
ragadabdu <ragadabdelwahab81@gmail.com>
2025-10-06 00:20:23 +0300

    feature: save results into a csv

--- web-scraper/web-scarper.py
+++ web-scraper/web-scarper.py
@@ -1,13 +1,16 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
+
+jobs = []
 
 response = requests.get("https://weworkremotely.com/categories/remote-full-stack-programming-jobs#job-listings")
 soup = BeautifulSoup(response.text, "lxml")
 
 # Find the first job card
-jobs = soup.find_all("li", class_="new-listing-container feature")
+jobs_card = soup.find_all("li", class_="new-listing-container feature")
 
-for job in jobs:
+for job in jobs_card:
     job_title = job.find("h3", class_="new-listing__header__title").text
     job_company_name = job.find("p", class_="new-listing__company-name").text
     job_location = job.find("p", class_="new-listing__company-headquarters").text
@@ -15,7 +18,6 @@ for job in jobs:
     job_type = ""
     job_salary= ""
     job_link = "https://weworkremotely.com"+job.find("a",href=True)["href"]
-    print(job_link)
     
     job_types = ["Full-Time", "Contract"]
     infos = job.find_all("p", class_="new-listing__categories__category")
@@ -28,6 +30,14 @@ for job in jobs:
         elif "$" not in text:
             job_salary = "not disclosed"
 
-    print(f'job title: {job_title}\ncompany: {job_company_name}\nsalary: {job_salary}\nloaction: {job_location}\nJob type: {job_type}\nposting date: {job_posting_date}\nlink: {job_link}\n\n')
+    jobs.append({"title": job_title, "company": job_company_name, "salary": job_salary, "location": job_location, "type": job_type, "post-date": job_posting_date, "link": job_link})
+    
+    print(f'job title: {job_title}\ncompany: {job_company_name}\nsalary: {job_salary}\nlocation: {job_location}\nJob type: {job_type}\nposting date: {job_posting_date}\nlink: {job_link}\n\n')
 
 
+with open("jobs.csv", "w", newline= "", encoding = "utf-8") as csvfile:
+    fieldnames = ["title", "company", "salary", "location", "type", "post-date", "link"]
+    writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+
+    writer.writeheader()
+    writer.writerows(jobs)
